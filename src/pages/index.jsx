7bdebb0a42c450d9b4c5f7606ef836b1127f4687
Gilbert3052 <gilbertfernandez3052@gmail.com>
2023-07-02 13:30:25 -0400

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      <h1>Hello World</h1>
+      {/* Header */}
+    </div>
+  );
+}
