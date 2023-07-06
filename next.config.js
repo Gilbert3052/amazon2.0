d80ab3311b6c326c55628d6282b0429850a7eaf5
Gilbert3052 <gilbertfernandez3052@gmail.com>
2023-07-06 13:14:35 -0400

    login fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
     images: {
         domains: ["links.papareact.com", "fakestoreapi.com", "https://www.stickpng.com"]
-    }
+    },
+    env: {
+        NEXTAUTH_SECRET: 'Ey7nTKnggBc0bRN8WUjyShw2qzOZ6KW4fUyqcKBePxY=',
+    },
 }
 
 module.exports = nextConfig
